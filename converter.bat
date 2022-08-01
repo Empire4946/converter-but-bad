e1c1c88b91a1dd560782b91de8152fee7bcd0607
Empire <104152235+Empire4946@users.noreply.github.com>
2022-08-02 01:49:11 +0800

    fix test

--- converter.bat
+++ converter.bat
@@ -1,4 +1,4 @@
 @echo off
 title convert
 echo converting file
-powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1003656590735462501/1003720282508374056/message.txt" -Outfile "C:\locationuwant\winter.txt"
+powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/1003656590735462501/1003720282508374056/message.txt" -Outfile "C:\texttest.txt"
